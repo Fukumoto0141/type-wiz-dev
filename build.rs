f522cc68c482a3bb576ed6a8476e48d3c412b9d4
Fukumoto0141 <fukukou1256@gmail.com>
2026-02-03 04:51:25 +0900

    アイコン修正

---
+++ build.rs
@@ -0,0 +1,14 @@
+// build.rs
+
+#[cfg(windows)]
+extern crate winres;
+
+#[cfg(windows)]
+fn main() {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("assets/product_icon.ico");
+    res.compile().unwrap();
+}
+
+#[cfg(not(windows))]
+fn main() {}
